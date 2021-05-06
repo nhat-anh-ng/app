0dac695b3be4ff79fccabb2fbd403d410d34f416
Nhat <nhatanh1nguyen@gmail.com>
2021-05-06 16:57:31 +0200

    pages setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <h4>Cocktailly</h4>
+        </Link>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
